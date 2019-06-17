9f31bde2b134fec104df716ffb3b9058a47a7202
Paul Rothrock <paul@movetoiceland.com>
2019-06-17 15:52:01 -0400

    Updated login method
    Signed-off-by: Paul Rothrock <paul@movetoiceland.com>

--- channel_check.rb
+++ channel_check.rb
@@ -7,8 +7,46 @@ require './lib/mattermost_api.rb'
 
 $config = YAML.load(
 	File.open('conf.yaml').read
-)
+)['mattermost_api']
 
-mm = MattermostApi.new($config['mattermost_api']['url'],
-				 	   $config['mattermost_api']['username'],
-				 	   $config['mattermost_api']['password'])
+mm = MattermostApi.new(mattermost_url: $config['url'], 
+						login_id: $config['username'],
+						password: $config['password'])
+
+
+if ARGV.length < 1
+	puts "Must be called with at least one argument"
+	exit!
+end
+
+# First argument should be the import file
+file_path = ARGV[0]
+
+if File.file?(file_path)
+	import_file = File.open(file_path, 'r')
+else
+	puts "#{file_path} does not exist"
+	exit!
+end
+
+if !ARGV[1].nil? and ARGV[1] == '--apply'
+	update_users = true
+else
+	update_users = false
+end
+
+import_file.each_line do |line|
+	json_line = JSON.parse(line)
+
+	if json_line['type'] != 'user'
+		next
+	end
+
+	pp json_line
+
+	if update_users
+		# Update the user
+	end
+end
+
+import_file.close
--- lib/mattermost_api.rb
+++ lib/mattermost_api.rb
@@ -3,6 +3,7 @@ require 'time'
 require 'digest'
 require 'pp'
 require 'fileutils'
+require 'uri'
 
 class MattermostApi
 	include HTTParty
@@ -10,44 +11,59 @@ class MattermostApi
 	format :json
 	# debug_output $stdout
 	
-	def initialize(mattermost_url, login_id, password)
-		@base_uri = mattermost_url + 'api/v4'
-		@login_id = login_id
-		@password = password
-
-		tmp_dir = './tmp/'
-
-		unless File.directory?(tmp_dir)
-		  FileUtils.mkdir_p(tmp_dir)
+	def initialize(mattermost_url: nil, login_id: nil, password: nil, token: nil)
+		if mattermost_url.nil?
+			raise 'mattermost_url must be set'
+		else
+			# Parse the URL
+			uri = URI(mattermost_url)
+			
+			@base_uri = uri.scheme + '://' + uri.host + ':' + uri.port.to_s + '/api/v4/'
+			
 		end
 
-		@tmp_file = tmp_dir + Digest::MD5.hexdigest("#{login_id}")
-
-		@options = {
-			headers: {
-				'Content-Type' => 'application/json',
-				'User-Agent' => 'Mattermost-HTTParty'
-			},
-			# TODO Make this more secure
-			verify: false
-		}
-		
-		token = nil
-		
-		begin
-			if File.file?(@tmp_file) && File.readable?(@tmp_file)
-				token = JSON.parse(File.read(@tmp_file))
-				if Time.now < Time.parse(token['expiration'])
-					token = token['value']
-				end
-			end
-		rescue Exception => e
-			pp e
+		if login_id.nil? and password.nil? and token.nil?
+			raise 'Set either login_id and password or token'
 		end
-
+		
 		if token.nil?
-			token = handle_login	
-		end
+			@login_id = login_id
+			@password = password
+
+			tmp_dir = './tmp/'
+
+			unless File.directory?(tmp_dir)
+			  FileUtils.mkdir_p(tmp_dir)
+			end
+
+			@tmp_file = tmp_dir + Digest::MD5.hexdigest("#{login_id}")
+
+			@options = {
+				headers: {
+					'Content-Type' => 'application/json',
+					'User-Agent' => 'Mattermost-HTTParty'
+				},
+				# TODO Make this more secure
+				verify: false
+			}
+			
+			token = nil
+			
+			begin
+				if File.file?(@tmp_file) && File.readable?(@tmp_file)
+					token = JSON.parse(File.read(@tmp_file))
+					if Time.now < Time.parse(token['expiration'])
+						token = token['value']
+					end
+				end
+			rescue Exception => e
+				pp e
+			end
+
+			if token.nil?
+				token = handle_login	
+			end
+		end		
 		
 		@options[:headers]['Authorization'] = "Bearer #{token}"
 		@options[:body] = nil
@@ -109,18 +125,10 @@ class MattermostApi
 
 		headers = login_response.headers
 
-		token = headers['Token']
-
-		cookie_hash = CookieHash.new
-
-		login_response.get_fields('Set-Cookie').each do |c|
-			cookie_hash.add_cookies(c)
-		end
-		
-		token_data = {value: token, expiration: cookie_hash[:Expires]}
-
-		if File.writable?(@tmp_file) || !File.file?(@tmp_file)
-			File.write(@tmp_file, token_data.to_json)
+		if headers['Token'].kind_of?(Array)
+			token = headers['Token'][0]
+		else
+			token = headers['Token'][0]
 		end
 
 		token
